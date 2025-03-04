ca843e7cb0ee572de4b4943bd8795bf069b4af78
Juliana Lindebeck <juliana.lindebeck@estudante.ufjf.br>
2025-03-04 14:14:34 -0300

    importa e imprime funções getMaria/João

---
+++ src/main.js
@@ -0,0 +1,8 @@
+// main.js
+console.log("módulo main");
+
+import { getMaria, getJoao } from "./joaoEMaria";
+
+console.log('Maria tem', getMaria(), 'maçã(s)!');
+console.log('João tem', getJoãoa(), 'maçã(s)!');
+
